44d7f2c803f248ab9a743d83821c228a924af315
Asma <158836479+asma675@users.noreply.github.com>
2025-12-24 19:26:45 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App.jsx";
 import "@/index.css";
-
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
